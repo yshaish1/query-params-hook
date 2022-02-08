fcb44c93810fe7791aeff3a63bfd8bfbf3e6904d
Yossi Shaish <yossi.shaish@sciplay.com>
2022-02-08 10:48:50 +0200

    add more param to test

--- lib/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { useQueryParams } from './useQueryParams';
--- src/__tests__/QueryParams.test.ts
+++ src/__tests__/QueryParams.test.ts
@@ -1,7 +1,8 @@
 import useQueryParams from '../index';
 
 test('Check Query Params', () => {
-  const { param1 } = useQueryParams();
+  const { param1, param2 } = useQueryParams();
 
   expect(param1).toBe('this is param 1');
+  expect(param2).toBe('this is param 2');
 });
